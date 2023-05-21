3c815e3c09badd4b495f64fe242fdac21bf23073
Jóannes í Sandagerði <joannes@sandagerdi.com>
2023-05-21 09:48:21 +0200

    Layout.

--- src/app/has-role.component.ts
+++ src/app/has-role.component.ts
@@ -9,29 +9,15 @@ import { SignalUserSwitcherComponent } from './signal-user-switcher/signal-user-
 @Component({
   selector: 'app-has-role-component',
   standalone: true,
-  imports: [CommonModule, HasRoleDirective, SignalHasRoleDirective, UserSwitcherComponent, SignalUserSwitcherComponent],
+  imports: [
+    CommonModule,
+    HasRoleDirective,
+    SignalHasRoleDirective,
+    UserSwitcherComponent,
+    SignalUserSwitcherComponent,
+  ],
   providers: [UserStore],
-  template: `
-    <h1>NGRX Component Store</h1>
-    <app-user-switcher></app-user-switcher>
-    
-    <h2>Information Panel</h2>
-    <p *hasRoleIsAdmin="true">visible only for super admin</p>
-    <p *hasRole="'MANAGER'; isAdmin: true">visible if manager</p>
-    <p *hasRole="['MANAGER', 'READER']">visible if manager and/or reader</p>
-    <p *hasRole="['MANAGER', 'WRITER']">visible if manager and/or writer</p>
-    <p *hasRole="'CLIENT'">visible if client</p>
-    <p>Visible for everyone</p>
-
-    <h1>Signal Store</h1>
-    <signal-app-user-switcher></signal-app-user-switcher>
-    <h2>Information Panel</h2>
-    <p *signalHasRoleIsAdmin="true">visible only for super admin</p>
-    <p *signalHasRole="'MANAGER'; isAdmin: true">visible if manager</p>
-    <p *signalHasRole="['MANAGER', 'READER']">visible if manager and/or reader</p>
-    <p *signalHasRole="['MANAGER', 'WRITER']">visible if manager and/or writer</p>
-    <p *signalHasRole="'CLIENT'">visible if client</p>
-    <p>Visible for everyone</p>
-  `
+  templateUrl: './has-role.component.html',
+  styleUrls: ['./has-role.component.scss']
 })
 export class HasRoleComponentComponent {}
